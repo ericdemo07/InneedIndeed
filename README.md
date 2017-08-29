f10e6aba1c84432ea8f97996658371b38375e678
ayush.shukla <ayush.shukla@rakuten.com>
2017-08-29 17:58:41 +0530

    added rnd with hometab.ts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # For DEV :
 
 ## StartMongoDB
-"C:\E_Drive\Tools\mongo\bin\mongod.exe" --dbpath "C:\E_Drive\GitRepositories\InneedIndeed\data"
+"C:\E_Drive\Tools\mongo\bin\mongod.exe" --dbpath "C:\E_Drive\GitRepositories\dbhouse\data"
--- src/app/home/home_tab.ts
+++ src/app/home/home_tab.ts
@@ -9,43 +9,37 @@ import { Person } from '../_model/person';
 })
 
 export class HomeTabComponent {
-  constructor(private defaultService: DefaultService) { }
+  listOfPerson1?: Person[];
+  person1:Person;
+  constructor(private defaultService: DefaultService) { this.listOfPerson1 = new Array<Person>();  }
   listOfPerson: Person[];
 
   getHeroes(): void {
-    this.defaultService.getInneedAsList1().then(listOfPerson => this.listOfPerson = listOfPerson);
+    this.defaultService.getInneedAsList1().then(listOfPerson11 => this.listOfPerson = listOfPerson11);
   }
   ngOnInit(): void {
     this.getHeroes();
   }
   test(): void {
     var listOfPersonGet: Person[] = [];
-    //a.push("doom");
     this.defaultService.getInneedAsList().then(
       function(response) {
-        //  console.log("Hello test  "+response);
         var length = response.message.length;
-        //console.log("\n\n\nHello test 111 " + JSON.parse(response).message.length);
-        console.log("\n\n" + 0 + "\n\n" + response.message.length);
         var person;
         for (var i = 0; i < 1; i++) {
-          //this.listOfPerson[i] = JSON.parse(response).message[i];
           person = new Person(response.message[i]._id, response.message[i].firstName, response.message[i].lastName, response.message[i].city, response.message[i].state,
             response.message[i].postalCode, response.message[i].addressLine1, response.message[i].addressLine2, response.message[i].donationAmount,
             response.message[i].content, response.message[i].img, response.message[i].mailId, response.message[i].phone,
             response.message[i].representativeId, response.message[i].likesCount, response.message[i].shareCount, response.message[i].dob);
-          console.log("\n\n" + i + "\n\n" + JSON.stringify(response.message[i]));
           console.log("\n\n" + i + "\n\n" + person.getFullName());
           listOfPersonGet.push(person);
         }
-console.log(listOfPersonGet.length);
-        //next line nopt giving expected result have to use setters
-        //    var myObjArray : Person[] = JSON.parse(response).message;
-        //  console.log(myObjArray[0]);
-        //  console.log(myObjArray[0].getImg);
-        //  console.log(myObjArray[0].getImg());
-
-        //MapUtils.deserialize(Person, JSON.parse(response).message);
-      });
+        console.log(listOfPersonGet.length);
+        console.log(listOfPersonGet[0]);
+      //  person1 => this.person1 = listOfPersonGet[0];
+        //console.log(this.listOfPerson1[0]);
+      //  return
+      }
+    );
   }
 }
